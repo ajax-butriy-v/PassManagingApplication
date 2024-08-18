247708efe00a32a2507b4f5145c877453dc98e09
ajax-butriy-v <butriy.v@ajax.systems>
2024-08-18 19:49:40 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/pass_manager/PassManagingApplication.kt
@@ -0,0 +1,11 @@
+package com.example.pass_manager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PassManagingApplication
+
+fun main(args: Array<String>) {
+	runApplication<PassManagingApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/pass_manager/PassManagingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.pass_manager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PassManagingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
