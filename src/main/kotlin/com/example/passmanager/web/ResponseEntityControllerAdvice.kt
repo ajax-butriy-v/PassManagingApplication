f2083524fbe94f94e838552afc1da5734f0176c2
ajax-butriy-v <butriy.v@ajax.systems>
2024-08-28 10:02:51 +0300

    Added runtime exceptions for @RestControllerAdvice

--- src/main/kotlin/com/example/passmanager/web/ResponseEntityControllerAdvice.kt
+++ src/main/kotlin/com/example/passmanager/web/ResponseEntityControllerAdvice.kt
@@ -1,7 +1,10 @@
 package com.example.passmanager.web
 
 import com.example.passmanager.exception.InvalidIdTypeException
+import com.example.passmanager.exception.PassOwnerNotFoundException
+import com.example.passmanager.exception.PassTypeNotFoundException
 import com.example.passmanager.web.dto.ExceptionResponse
+import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -13,4 +16,12 @@ class ResponseEntityControllerAdvice {
     fun handleBadRequest(runtimeException: RuntimeException): ResponseEntity<ExceptionResponse> {
         return ResponseEntity.badRequest().body(ExceptionResponse(runtimeException.message ?: ""))
     }
+
+    @ExceptionHandler(
+        value = [PassTypeNotFoundException::class, PassOwnerNotFoundException::class, PassTypeNotFoundException::class]
+    )
+    fun handleNotFound(runtimeException: RuntimeException): ResponseEntity<ExceptionResponse> {
+        return ResponseEntity.status(NOT_FOUND).body(ExceptionResponse(runtimeException.message ?: ""))
+    }
 }
+
