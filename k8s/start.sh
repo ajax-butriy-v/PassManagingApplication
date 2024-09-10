ee17fe5f78443098cd51dcc2bbd001dc53c35f2b
ajax-butriy-v <butriy.v@ajax.systems>
2024-09-10 17:44:12 +0300

    Added Kubernetes components

---
+++ k8s/start.sh
@@ -0,0 +1,16 @@
+eval $(minikube docker-env)
+docker build -t passmanagingapplication-app -f ~/IdeaProjects/PassManagingApplication/Dockerfile ~/IdeaProjects/PassManagingApplication
+
+minikube addons enable ingress
+
+
+kubectl apply -f mongo-secret.yaml
+kubectl apply -f mongo-configmap.yaml
+kubectl apply -f app-configmap.yaml
+kubectl apply -f mongo.yaml
+kubectl apply -f mongo-express.yaml
+
+kubectl wait --for=condition=ready pod -l app=mongodb --timeout=300s
+
+kubectl apply -f app.yaml
+kubectl apply -f app-ingress.yaml
