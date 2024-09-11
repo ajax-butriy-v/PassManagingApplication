eca299d8baadad2fe38a08bbc663a6a99d9d30c8
ajax-butriy-v <butriy.v@ajax.systems>
2024-09-11 16:09:50 +0300

    Modified k8s configs

--- k8s/start.sh
+++ k8s/start.sh
@@ -5,12 +5,11 @@ minikube addons enable ingress
 
 
 kubectl apply -f mongo-secret.yaml
-kubectl apply -f mongo-express-configmap.yaml
 kubectl apply -f app-configmap.yaml
 kubectl apply -f mongo.yaml
-kubectl apply -f mongo-express.yaml
 
 kubectl wait --for=condition=ready pod -l app=mongodb --timeout=300s
 
+kubectl apply -f mongo-express.yaml
 kubectl apply -f app.yaml
 kubectl apply -f app-ingress.yaml
