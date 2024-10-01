ae3df33610599002ccc854b868036b549140a700
ajax-butriy-v <butriy.v@ajax.systems>
2024-10-01 23:49:33 +0300

    Rewritten Mongock to reactive mongo template

--- src/main/kotlin/com/example/passmanager/migration/InitialIndexCreationChangeUnit.kt
+++ src/main/kotlin/com/example/passmanager/migration/InitialIndexCreationChangeUnit.kt
@@ -9,15 +9,14 @@ import org.bson.Document
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.data.domain.Sort.Direction.ASC
-import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
 import org.springframework.data.mongodb.core.index.CompoundIndexDefinition
 import org.springframework.data.mongodb.core.index.Index
 import org.springframework.data.mongodb.core.indexOps
+import reactor.core.publisher.Flux
 
 @ChangeUnit(id = "Create indexes", order = "1", author = "Vitalii Butriy")
-internal class InitialIndexCreationChangeUnit(private val mongoTemplate: MongoTemplate) {
-    private lateinit var passOwnerIdAndPurchasedAtIndexName: String
-    private lateinit var passOwnerIndexNames: List<String>
+internal class InitialIndexCreationChangeUnit(private val mongoTemplate: ReactiveMongoTemplate) {
 
     @Execution
     fun createIndexes() {
@@ -26,14 +25,11 @@ internal class InitialIndexCreationChangeUnit(private val mongoTemplate: MongoTe
                 .append(MongoPass::passOwnerId.name, 1)
                 .append(MongoPass::purchasedAt.name, 1)
         )
-        mongoTemplate.indexOps<MongoPass>().ensureIndex(passOwnerIdAndPurchasedAtIndexDefinition).also { indexName ->
-            passOwnerIdAndPurchasedAtIndexName = indexName
-        }
-        passOwnerIndexNames = mongoTemplate.indexOps<MongoPassOwner>().run {
-            listOf(
-                ensureIndex(Index(MongoPassOwner::email.name, ASC).unique()),
-                ensureIndex(Index(MongoPassOwner::phoneNumber.name, ASC).unique())
-            )
+        mongoTemplate.indexOps<MongoPass>().ensureIndex(passOwnerIdAndPurchasedAtIndexDefinition).subscribe()
+
+        mongoTemplate.indexOps<MongoPassOwner>().apply {
+            ensureIndex(Index(MongoPassOwner::email.name, ASC).unique()).subscribe()
+            ensureIndex(Index(MongoPassOwner::phoneNumber.name, ASC).unique()).subscribe()
         }
     }
 
@@ -41,21 +37,25 @@ internal class InitialIndexCreationChangeUnit(private val mongoTemplate: MongoTe
     fun dropIndexesInCaseOfFailure() {
         log.info("Rolling back ${this::class.simpleName}")
 
-        val allPassIndexNames = indexNames<MongoPass>()
-        if (allPassIndexNames.contains(passOwnerIdAndPurchasedAtIndexName)) {
-            mongoTemplate.indexOps<MongoPass>().dropIndex(passOwnerIdAndPurchasedAtIndexName)
-        }
-
-        val passOwnerIndexOperations = mongoTemplate.indexOps<MongoPassOwner>()
-        passOwnerIndexNames.filter { it in indexNames<MongoPassOwner>() }
-            .forEach { passOwnerIndexOperations.dropIndex(it) }
+        Flux.merge(indexNamesFlux<MongoPass>(), indexNamesFlux<MongoPassOwner>())
+            .filter { indexNameToCollectionNameMap.containsKey(it) }
+            .flatMap { indexName ->
+                val value = indexNameToCollectionNameMap.get(indexName).orEmpty()
+                mongoTemplate.indexOps(value).dropIndex(indexName)
+            }
+            .subscribe()
     }
 
-    private inline fun <reified T> indexNames(): List<String> {
+    private inline fun <reified T> indexNamesFlux(): Flux<String> {
         return mongoTemplate.indexOps(T::class.java).indexInfo.map { it.name }
     }
 
     companion object {
         val log: Logger = LoggerFactory.getLogger(InitialIndexCreationChangeUnit::class.java)
+        val indexNameToCollectionNameMap: Map<String, String> = mapOf(
+            "passOwnerId_1_purchasedAt_1" to MongoPass.COLLECTION_NAME,
+            "email_1" to MongoPassOwner.COLLECTION_NAME,
+            "phoneNumber_1" to MongoPassOwner.COLLECTION_NAME
+        )
     }
 }
