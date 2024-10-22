fa8f9cd4f962ad18442694fc4cb36f5d3bb01614
ajax-butriy-v <butriy.v@ajax.systems>
2024-10-22 15:57:59 +0300

    Implemented K8s files for NATS + removed mongo express

--- k8s/shutdown.sh
+++ k8s/shutdown.sh
@@ -1,3 +1,8 @@
-for file in *.yaml; do
-  kubectl delete -f "$file"
-done
+kubectl delete -f app.yaml
+kubectl delete -f app-configmap.yaml
+kubectl delete -f app-ingress.yaml
+kubectl delete -f gateway.yaml
+kubectl delete -f gateway-ingress.yaml
+kubectl delete -f mongo.yaml
+kubectl delete -f mongo-secret.yaml
+kubectl delete -f nats.yaml
