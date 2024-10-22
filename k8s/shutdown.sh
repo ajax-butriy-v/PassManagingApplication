959a711bc0736eb85582bbe93cee3f4045948de5
ajax-butriy-v <butriy.v@ajax.systems>
2024-10-22 16:48:42 +0300

    Small changes

--- k8s/shutdown.sh
+++ k8s/shutdown.sh
@@ -1,8 +1,8 @@
-kubectl delete -f app.yaml
-kubectl delete -f app-configmap.yaml
-kubectl delete -f app-ingress.yaml
-kubectl delete -f gateway.yaml
-kubectl delete -f gateway-ingress.yaml
-kubectl delete -f mongo.yaml
-kubectl delete -f mongo-secret.yaml
-kubectl delete -f nats.yaml
+kubectl delete -f app.yaml \
+               -f app-configmap.yaml \
+               -f app-ingress.yaml \
+               -f gateway.yaml \
+               -f gateway-ingress.yaml \
+               -f mongo.yaml \
+               -f mongo-secret.yaml \
+               -f nats.yaml \
