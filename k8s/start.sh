d78afc083867f526a8b0ba199ef6eaf85a492c77
Vitalii Butriy <butriy.v@ajax.systems>
2024-11-06 18:49:42 +0200

    K8s for Kafka and Grpc (#12)

--- k8s/start.sh
+++ k8s/start.sh
@@ -7,14 +7,19 @@ minikube addons enable ingress
 
 kubectl apply -f mongo-secret.yaml \
               -f app-configmap.yaml \
+              -f kafka-configmap.yaml \
+              -f zookeeper-configmap.yaml \
               -f mongo.yaml \
               -f mongo-volume.yaml \
               -f mongo-pvc.yaml \
-              -f nats.yaml
+              -f nats.yaml \
+              -f zookeeper.yaml \
+              -f kafka.yaml \
+
 
 kubectl wait --for=condition=ready pod -l app=mongodb --timeout=300s
+kubectl wait --for=condition=ready pod -l app=kafka --timeout=300s
 
 kubectl apply -f app.yaml \
-              -f app-ingress.yaml \
               -f gateway.yaml \
-              -f gateway-ingress.yaml
+              -f ingress.yaml
