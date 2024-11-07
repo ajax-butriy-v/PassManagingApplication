de8540bf88fc0f4145c28979f03893e368e5a396
ajax-butriy-v <butriy.v@ajax.systems>
2024-11-07 15:08:42 +0200

    Modified repository tests

---
+++ core/src/main/kotlin/com/example/core/util/ExceptionUtil.kt
@@ -0,0 +1,13 @@
+package com.example.core.util
+
+import io.lettuce.core.RedisException
+import org.springframework.data.redis.RedisConnectionFailureException
+import java.net.SocketException
+
+fun isRedisOrSocketException(error: Throwable): Boolean {
+    return listOf(
+        RedisConnectionFailureException::class.java,
+        RedisException::class.java,
+        SocketException::class.java,
+    ).any { it.isInstance(error) }
+}
